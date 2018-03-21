2254b4d0d9ced8b11cf724a7e08277639262e211
Praveenachandra <36299134+Praveenachandra@users.noreply.github.com>
2018-03-21 15:27:44 +0530

    Update armstrong.py

--- armstrong.py
+++ armstrong.py
@@ -1,9 +1,10 @@
 a=int(input(""))
 temp=a
 sum=0
+order=len(str(a))
 while (temp>0):
     digit=temp%10
-    sum+=digit**3
+    sum+=digit**order
     temp//=10
 if (sum==a):
     print (a,"is a armstrong number")
