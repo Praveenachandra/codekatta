aca353b717f64d875a13e50c2b3373f65a67a6de
Praveenachandra <36299134+Praveenachandra@users.noreply.github.com>
2018-04-14 11:18:10 +0530

    Create words.py

---
+++ words.py
@@ -0,0 +1,18 @@
+import math
+number = int(input())
+number_list = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+teen_list = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+decades_list =["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+if number <= 9:
+    print(number_list[number].capitalize())
+elif number >= 10 and number <= 19:
+    tens = number % 10
+    print(teen_list[tens].capitalize())
+elif number > 19 and number <= 99:
+    ones = math.floor(number/10)
+    twos = ones - 2
+    tens = number % 10
+    if tens == 0:
+        print(decades_list[twos].capitalize())
+    elif tens != 0:
+        print(decades_list[twos] + " " + number_list[tens])
